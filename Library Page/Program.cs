e1b6d32966dbba3aec924968adfe04be615f7532
NeumontJack <100381502+NeumontJack@users.noreply.github.com>
2023-03-02 17:41:00 -0700

    Image Picker

--- Library Page/Program.cs
+++ Library Page/Program.cs
@@ -14,7 +14,7 @@ namespace Library_Page
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Library());
+            Application.Run(new Images());
         }
     }
 }
